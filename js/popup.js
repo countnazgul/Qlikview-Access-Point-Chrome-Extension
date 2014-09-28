35a269728059f6e5c2fa9d8e6d5490456d388ec0
Stefan Stoichev <stefan.stoichev@gmail.com>
2014-09-28 06:05:27 +0100

    v1.3 * Multi access points - display documents from many access points at once * Extended search - you can search by document category, server name tag, server url (+ document name ofc.) * Coloring - define color code based on the server - in the list with document this color will be shown * Code cleanup * Fixes

--- js/popup.js
+++ js/popup.js
@@ -4,148 +4,202 @@ var username = '';
 var password = '';
 jQuery("#error").hide();
 jQuery("#loading").show();
+errors = [];
+toprocess = 0;
+allprocessed = 0;
 
 $(document).ready(function() {
-  server = store.get("server");
-  username = store.get("username");
-  password = store.get("password");
   
-  if(server === undefined || username === undefined || password === undefined) {
-    jQuery("#loading").hide();
-    jQuery("#error").show();
-    $("#error").text('Some of the options are empty. Please check the options page.');
+  servers = store.get('servers');
+  if(!servers) {
+    AllProcessed();
+    errors.push('No servers defined. Use Options page.');
+    Errors();
   } else {
+    servers = JSON.parse(servers);
+    if(servers.length > 0) {
     
+    toprocess = servers.length;
       // $(".signOutLink").click(function() {
       //     var e = '<update mark="" stamp="" cookie="true" scope="Document" ident="AccessPoint" kind="AccessPoint"><set name="AccessPoint.Logout" action="" clientsizeWH="1280:939" /></update>';
       //     $.post(url, e, function(f) {
       //         location.reload()
       //     })
       // })    
+      
+      jQuery("#loading").show();
+      jQuery("#error").hide();
+      favorite = '<img src="images/box_star.png" height="14" width="14" style="position: absolute; left: 0px; margin-top: 8px">';
+      ajaxImg = '<img src="images/icon_ajax.png" height="14" width="14" style="position: absolute; right: 45px; margin-top: 7px;opacity: 0.4;">';
+      ieImg = '<img src="images/icon_ie.png" height="14" width="14" style="position: absolute; right: 15px; margin-top: 7px; opacity: 0.4;">';
+      mobileImg = '<img src="images/icon_mobile.png" height="14" width="14" style="position: absolute; right: 31px; margin-top: 7px; opacity: 0.5;">';
     
-    jQuery("#loading").show();
-    jQuery("#error").hide();
-    favorite = '<img src="images/box_star.png" height="14" width="14" style="position: absolute; left: 0px; margin-top: 8px">';
-    ajaxImg = '<img src="images/icon_ajax.png" height="14" width="14" style="position: absolute; right: 31px; margin-top: 7px;opacity: 0.4;">';
-    ieImg = '<img src="images/icon_ie.png" height="14" width="14" style="position: absolute; right: 15px; margin-top: 7px; opacity: 0.4;">';
-    mobileImg = '<img src="images/icon_mobile.png" height="14" width="14" style="position: absolute; right: 45px; margin-top: 7px; opacity: 0.5;x">';
-  
-    var openUrl = server + '/QvAJAXZfc/AccessPoint.aspx?open=&id=';
-    var platform = 'browser.chrome';
-    var url = server + "/QvAJAXZfc/AccessPoint.aspx?mark=&platform=" + platform + "&dpi=96";
-
-    TryLogin(username, password, function(status) {
-      if(status != false) {
-        jQuery("#loading").show();
-        var a = '<update mark="" stamp="" cookie="true" scope="Document" ident="AccessPoint" kind="AccessPoint"><set name="AccessPoint.Update" action="" /><set name="Document" add="mode;ie6false" /><set name="AccessPoint.Pagesize" value="0" /></update>';
-        $.post(url, a)
-          .done(function( c ) {
-          //console.log(c)
-                xmlText = new XMLSerializer().serializeToString(c);
-                var x2js = new X2JS();
-                var jsonObj = x2js.xml_str2json( xmlText );
-                result = jsonObj.result.object.value;
-                for(i = 0; i < result.length; i++) {
-                  if( result[i]._name == 'Documents') {
-                    documents = result[i].element;
-                    //console.log(documents)
-                    for(d = 0; d < documents.length; d++) {
-                      ajax  = '';
-                      ajaxMobile = '';
-                      ie = '';
-                      qvdocument = documents[d];
-                      
-                      clientsArray = qvdocument._clients.split(';');
-                      for(c = 0; c < clientsArray.length; c++) {
-                        if(clientsArray[c] == 'Ajax') {
-                          ajax = ajaxImg;
-                        }
-                        
-                        if(clientsArray[c] == 'AjaxOnSmallDevices') {
-                          ajaxMobile = mobileImg;
-                        }
-                        
-                        if(clientsArray[c] == 'Plugin') {
-                          ie = ieImg;
-                        }                      
-                        
-                      }
-                      
-                      if(qvdocument._favorite) {
-                        fav = favorite
-                      } else {
-                        fav = '';
-                      }
-                                        
-                      availability = ajax + ' ' + ajaxMobile + ' ' + ie;
-                      qvdoc = qvdocument._text.replace('.qvw', '');
-                      if(qvdoc.length > 50) {
-                        qvdoc = qvdoc.substring(0,50) + ' ...';
-                      }
-                      $("#search_list").append('<div class="outer"><li><a title="QVW: ' + qvdocument._text + 
-                                                    '\nCategory: '+ qvdocument._category + 
-                                                    '\nSize: '+ qvdocument._filesize + 
-                                                    //'\nClients: ' + qvdocument._clients  +
-                                                    '\nLast Modified: ' + qvdocument._lastModified  +
-                                                    '\nLast Update: ' + qvdocument._lastUpdate  +
-                                                    '\nNext Update: ' + qvdocument._nextUpdate  +
-                                                    '" href="' + openUrl + '' + qvdocument._value + '&client=Ajax' + '" target="_blank">' + 
-                                                    fav + qvdoc + availability + '</a> <span style="display:none">' + qvdocument._category + '</span> </li></div>');
-                    }
-                    
-                    $('#search_input').fastLiveFilter('#search_list', { 
-                            callback:function(total) {
-                            $('#search_list').unhighlight();
-                            searchTerm = $("#search_input").val();
-                            if (searchTerm.length > 0) {
-                                $('#search_list').highlight(searchTerm);
+    
+      jQuery.eachAsync(servers, {
+              delay: 100,
+              bulk: 0,
+              loop: function(index, value)
+              {
+                server = value.server;
+                username = value.username;
+                password = value.password;
+                
+                var openUrl = server + '/QvAJAXZfc/AccessPoint.aspx?open=&id=';
+                var platform = 'browser.chrome';
+                var url = server + "/QvAJAXZfc/AccessPoint.aspx?mark=&platform=" + platform + "&dpi=96";
+          
+                TryLogin(username, password, function(status) {
+                  if(status != false) {
+                    //jQuery("#loading").show();
+                    var a = '<update mark="" stamp="" cookie="true" scope="Document" ident="AccessPoint" kind="AccessPoint"><set name="AccessPoint.Update" action="" /><set name="Document" add="mode;ie6false" /><set name="AccessPoint.Pagesize" value="0" /></update>';
+                    $.post(url, a)
+                      .done(function( c ) {
+                      //console.log(c)
+                            xmlText = new XMLSerializer().serializeToString(c);
+                            var x2js = new X2JS();
+                            var jsonObj = x2js.xml_str2json( xmlText );
+                            result = jsonObj.result.object.value;
+                            for(i = 0; i < result.length; i++) {
+                              if( result[i]._name == 'Documents') {
+                                documents = result[i].element;
+                                //console.log(documents)
+                                for(d = 0; d < documents.length; d++) {
+                                  ajax  = '';
+                                  ajaxMobile = '';
+                                  ie = '';
+                                  qvdocument = documents[d];
+                                  
+                                  clientsArray = qvdocument._clients.split(';');
+                                  for(c = 0; c < clientsArray.length; c++) {
+                                    if(clientsArray[c] == 'Ajax') {
+                                      ajax = ajaxImg;
+                                    }
+                                    
+                                    if(clientsArray[c] == 'AjaxOnSmallDevices') {
+                                      ajaxMobile = mobileImg;
+                                    }
+                                    
+                                    if(clientsArray[c] == 'Plugin') {
+                                      ie = ieImg;
+                                    }                      
+                                    
+                                  }
+                                  
+                                  if(qvdocument._favorite) {
+                                    fav = favorite
+                                  } else {
+                                    fav = '';
+                                  }
+                                                    
+                                  availability = ajax + ' ' + ajaxMobile + ' ' + ie;
+                                  qvdoc = qvdocument._text.replace('.qvw', '');
+                                  if(qvdoc.length > 50) {
+                                    qvdoc = qvdoc.substring(0,50) + ' ...';
+                                  }
+                                  
+                                  
+                                  var counter = 0;
+                                    for (var key in value) {
+                                      if(counter == 4) {
+                                        color = value[key];
+                                      }
+                                      counter++;
+                                    }
+                                  
+                                  r = hexToRgb(color).r;
+                                  g = hexToRgb(color).g;
+                                  b = hexToRgb(color).b;
+                                  
+                                  categoryColor = 'background: -webkit-gradient(linear, left top, right bottom, \
+                                                   color-stop(0%,rgba(' + r + ',' + g + ',' + b + ',0)), \
+                                                   color-stop(73%,rgba(' + r + ',' + g + ',' + b + ',0)), \
+                                                   color-stop(100%,rgba(' + r + ',' + g + ',' + b + ',1)));';
+                                  
+                                  $("#search_list").append('<div class="outer"><li><a title="QVW: ' + qvdocument._text +
+                                                                '\nServer: '+ value.server +
+                                                                '\nCategory: '+ qvdocument._category + 
+                                                                '\nSize: '+ qvdocument._filesize + 
+                                                                //'\nClients: ' + qvdocument._clients  +
+                                                                '\nLast Modified: ' + qvdocument._lastModified  +
+                                                                '\nLast Update: ' + qvdocument._lastUpdate  +
+                                                                '\nNext Update: ' + qvdocument._nextUpdate  +
+                                                                '" href="' + openUrl + '' + qvdocument._value + '&client=Ajax' + '" target="_blank">' + 
+                                                                '<span class="server" width: 14; style="' + categoryColor +' font-size:9px;"></span>' +
+                                                                fav + qvdoc + availability + '</a><span style="display:none">' + qvdocument._category + 
+                                                                '</span><span style="display:none">' + value.server + 
+                                                                '</span><span style="display:none">' + value.nametag + 
+                                                                '</span> </li></div>');
+                                }
+                                
+                                $('#search_input').fastLiveFilter('#search_list', { 
+                                        callback:function(total) {
+                                        $('#search_list').unhighlight();
+                                        searchTerm = $("#search_input").val();
+                                        if (searchTerm.length > 0) {
+                                            $('#search_list').highlight(searchTerm);
+                                        }
+                                    }
+                                });
+                                $("#search_input").on("search", function() {
+                                  if ($('#search_input').val() == "")
+                                    $('#search_input').val('').change();
+                                });
+                                
+                                websiteAddress = document.getElementById("search_input");
+                                websiteAddress.focus();
+                                allprocessed++;
+                                AllProcessed();
+                              }
+                              
                             }
-                        }
-                    });
-                    jQuery("#loading").hide();
-                    $("#search_input").on("search", function() {
-                      if ($('#search_input').val() == "")
-                        $('#search_input').val('').change();
-                    });
-                    
-                    websiteAddress = document.getElementById("search_input");
-                    websiteAddress.focus();                  
+                          
+                        })
+                          .fail(function() {
+                            allprocessed++;
+                            AllProcessed();
+                            errors.push(server);
+                            errors.push('Error reaching: ' + server);
+                            Errors();
+                            //console.log(errors);
+                          });
+                  } else {
+                    allprocessed++;
+                    AllProcessed();              
+                    errors.push('Error reaching: ' + server);
+                    Errors();
+                    //console.log(errors);
                   }
-                  
-                }
-              
-            })
-              .fail(function() {
-                jQuery("#loading").hide();
-                jQuery("#error").show();
-                $("#error").text('Communication error. Please try again later.');
-              });
+                })
+                  },
+                  end: function()
+                  {
+                          //console.log(' END');
+                  }
+          });
     } else {
-      jQuery("#loading").hide();
-      $("#error").text('Error reaching the server. Check options or network connection.');
+      AllProcessed();
       jQuery("#error").show();
+      errors.push('No servers defined. Use Options page.');
+      Errors();
     }
-  })
-}
+  }
 });    
   
   
 function TryLogin(username, password, callback) {
-   jQuery("#loading").show();
-   
-	 formData = {username : username, password: password };
+	formData = {username : username, password: password };
 
-if(server.indexOf('http:') > -1) {
-  server = server.replace('http://', "");
-  url = 'http://' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '@' + server + '/QvAJAXZfc/Authenticate.aspx?back=/qlikview/FormLogin.htm';
-} else {
-  if(server.indexOf('https:') > -1) {
-    server = server.replace('https://', "");
-    url = 'https://' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '@' + server + '/QvAJAXZfc/Authenticate.aspx?back=/qlikview/FormLogin.htm';
-  } else {
+  if(server.indexOf('http:') > -1) {
+    server = server.replace('http://', "");
     url = 'http://' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '@' + server + '/QvAJAXZfc/Authenticate.aspx?back=/qlikview/FormLogin.htm';
+  } else {
+    if(server.indexOf('https:') > -1) {
+      server = server.replace('https://', "");
+      url = 'https://' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '@' + server + '/QvAJAXZfc/Authenticate.aspx?back=/qlikview/FormLogin.htm';
+    } else {
+      url = 'http://' + encodeURIComponent(username) + ':' + encodeURIComponent(password) + '@' + server + '/QvAJAXZfc/Authenticate.aspx?back=/qlikview/FormLogin.htm';
+    }
   }
-}
 
 	 var ajaxObject = {
 	    url: url,
@@ -162,4 +216,37 @@ if(server.indexOf('http:') > -1) {
   		}
     };
 	$.ajax( ajaxObject );
+}
+
+function AllProcessed() {
+  if(allprocessed == toprocess) {
+    jQuery("#loading").hide();
+  }
+}
+
+function Errors(){
+
+  $("#error").html('');
+  text = '';
+  
+  for(i = 0; i < errors.length; i++) {
+    text = text + errors[i] + '<br/>';
+  }
+  
+  $("#error").html(text);  
+  jQuery("#error").show();
+}
+
+function hexToRgb(hex) {
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }
--- options/main.js
+++ options/main.js
@@ -18,54 +18,34 @@ $(document).ready(function() {
   });
 
    $("#save").live("click", function() {
-      // var server = document.getElementById('server').value;
-      // var username = document.getElementById('username').value;
-      // var password = document.getElementById('password').value;
-      // ValidateBeforeSave(server, username, password, function(validate) {
-      //   if(validate == true ) {
-      //       store.set('server', server);
-      //       store.set('username', username);
-      //       store.set('password', password);
-      //       $("#notif").text("Saved");
-      //       $("#notif").fadeOut(5000, function() {
-      //         $("#notif").text("");
-      //         $("#notif").css({display: "inline"});
-      //       });
-      //   } else {
-      //       $("#notif").text("Please fill all fields.");
-      //       $("#notif").fadeOut(5000, function() {
-      //         $("#notif").text("");
-      //         $("#notif").css({display: "inline"});
-      //       });            
-      //   }
-      // })
-      
-            // test = $( "#displaytable" ).find('table');
-            // for(t = 0; t < test.length; t++) {
-            //   inputs = $( test[t] ).find('table');  
-            //   console.log(inputs.length);
-            // }
-            
-            servers = [];
-            $( "#displaytable" ).find('table').each(function(i) {
-              var $this = $(this);
-              server = {};
-              $this.find('input').each(function(a) {
-                server[$(this).attr('id')] = $(this).val();
-              });
+     Save();
+            // servers = [];
+            // $( "#displaytable" ).find('table').each(function(i) {
+            //   var $this = $(this);
+            //   server = {};
+            //   $this.find('input').each(function(a) {
+            //     server[$(this).attr('id')] = $(this).val();
+            //   });
               
-              $this.find('button').each(function(a) {
-                server.id = $(this).attr('id');
-              });
-              //server.id = Math.uuid(17);
-              servers.push(server);
-            });
+            //   $this.find('button').each(function(a) {
+            //     server.id = $(this).attr('id');
+            //   });
+            //   //server.id = Math.uuid(17);
+            //   servers.push(server);
+            // });
             
-            store.set('servers', JSON.stringify(servers));
+            // store.set('servers', JSON.stringify(servers));
+            // $("#notif").text("Saved");
+            // $("#notif").fadeOut(5000, function() {
+            //   $("#notif").text("");
+            //   $("#notif").css({display: "inline"});
+            // });
     });
     
    $("#add").live("click", function() {
-      AppendServerSection('', '', '', Math.uuid(17));
+      AppendServerSection('', '', '', '', '', Math.uuid(17));
+      Save();
+      RemoveServer();
     });     
     
 });
@@ -74,22 +54,40 @@ function LoadFromStorage() {
     $("#displaytable").html('');
     servers = store.get('servers');
     if(servers) {
-    servers = JSON.parse(servers);
-       for(i = 0; i < servers.length; i++) {
-         AppendServerSection(servers[i].server, servers[i].username, servers[i].password, servers[i].id);
-       }
-       RemoveServer();
+      servers = JSON.parse(servers);
+         for(i = 0; i < servers.length; i++) {
+           
+          var counter = 0;
+          s = servers[i];
+          for (var key in s) {
+            if(counter == 4) {
+              color = s[key];
+              colorId = key;
+            }
+            counter++;
+          }
+          
+           AppendServerSection(servers[i].server, servers[i].username, servers[i].password, colorId, color ,servers[i].id);
+           //console.log(servers[i][3])
+         }
+      RemoveServer();
     }
+    
+    //jscolor.install();
 }
 
-function AppendServerSection(server, username, password, id) {
+function AppendServerSection(server, username, password, colorId, color ,id) {
+  if(!colorId) {
+    colorId = Math.uuid(17);
+  }
+  
   $( "#displaytable" ).append(' \
   <section> \
     <div> \
       <table><tr> \
 		      <td>Server</td> \
 		      <td><input id="server" value="'+ server  +'" type="text"  size="97" placeholder="QV server dns/ip in format: http://my-qv-server [add port if server is not listening on 80 (default)]"></td> \
-		      <td></td> \
+		      <td> </td> \
 		    </tr> \
 		    <tr> \
 		      <td>Username</td> \
@@ -99,11 +97,25 @@ function AppendServerSection(server, username, password, id) {
 		    <tr> \
 		      <td>Password</td> \
 		      <td><input id="password" value="'+ password  +'" type="password" placeholder="password"></td> \
-		      <td><button id="' + id + '" class="remove" style="margin-top: 10px">Remove</button></td> \
+		      <td></td> \
+		    </tr> \
+		    <tr> \
+		    <td>Identifier</td> \
+		    <td> <input id="nametag" placeholder="optional name tag"></td> \
+		    <td></td> \
+		    </tr> \
+		    <tr> \
+		    <td>Color</td> \
+		    <td> <input class="color" value="' + color +'" id="' + colorId + '"> (optional)</td> \
+		    <td><button id="' + id + '" class="remove" style="margin-top: 10px">Remove</button></td> \
 		    </tr> \
 		  </table> \
 		</div> \
 	</section>');
+	
+	input = document.getElementById(colorId);
+	col = new jscolor.color(input)
+	
 }
 
 function RemoveServer() {
@@ -122,8 +134,37 @@ function RemoveServer() {
         store.set('servers', JSON.stringify(result));
         $("#displaytable").html('');
 	      LoadFromStorage();
+        $("#notif").text("Saved");
+        $("#notif").fadeOut(5000, function() {
+          $("#notif").text("");
+          $("#notif").css({display: "inline"});
+        });	      
     }, false);
 	}  
+}
+
+function Save() {
+  servers = [];
+  $( "#displaytable" ).find('table').each(function(i) {
+    var $this = $(this);
+    server = {};
+    $this.find('input').each(function(a) {
+      server[$(this).attr('id')] = $(this).val();
+    });
+    
+    $this.find('button').each(function(a) {
+      server.id = $(this).attr('id');
+    });
+    //server.id = Math.uuid(17);
+    servers.push(server);
+  });
+  
+  store.set('servers', JSON.stringify(servers));
+  $("#notif").text("Saved");
+  $("#notif").fadeOut(5000, function() {
+    $("#notif").text("");
+    $("#notif").css({display: "inline"});
+  });
 }
 
 function ValidateBeforeSave(server, username, password, callback) {
